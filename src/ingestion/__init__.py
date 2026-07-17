f661e10029a844a721dc2a505d9b7f93c85918f5
Rajarshi ghosh <rajarshighosh6228@gmail.com>
2026-07-17 20:37:03 +0530

    initialization files implemented

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -1,1 +1,11 @@
-#
+
+
+from .loader import DataLoader
+from .validator import DataValidator
+from .cleaner import DataCleaner
+
+__all__ = [
+    "DataLoader",
+    "DataValidator",
+    "DataCleaner",
+]
